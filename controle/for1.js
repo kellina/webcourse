1ac26b96bc3b028e2f4ce6bb58bbfb91265c1d01
Kellina Oliveira de Souza <kellina@gmail.com>
2020-02-24 15:31:28 -0300

    Using For

---
+++ controle/for1.js
@@ -0,0 +1,14 @@
+let contador = 1  //declação da variável
+while (contador <= 10) {  //expressão
+  console.log (`contador = ${contador}`)
+  contador++  //incremento
+}
+
+for (let i = 1; i <= 10; i++) {
+  console.log(`i = ${i}`)
+}
+
+const notas = [6.7, 7.4, 9.8, 8.1, 7.7]
+for (let i = 0; i < notas.length; i++) {
+  console.log(`nota = ${notas[i]}`)
+}
